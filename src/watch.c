fd4257cc0885213cd67eab85ef72e5c7e7ebcb5f
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-11-16 00:11:03 +0200

    add executable command

---
+++ src/watch.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <unistd.h>
+//#include <time.h>
+//#include <fcntl.h>
+//#include <sys/wait.h>
+//#include "ms/ms.h"
+
+int main(int argc, const char **argv) {
+    printf("watch test");
+    return 0;
+}
