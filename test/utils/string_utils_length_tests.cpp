ece13dacee63c81977c01e6f776ba504efa68e31
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-11-24 21:55:16 +0200

    length tests

--- test/utils/string_utils_length_tests.cpp
+++ test/utils/string_utils_length_tests.cpp
@@ -4,21 +4,28 @@ extern "C" {
 #include "utils/string_utils.h"
 }
 
-//TEST(utils, add) {
-//    // Arrange
-//    double res;
-//    // Act
-//    res = add_numbers(1.0, 2.0);
-//    // Assert
-//    ASSERT_NEAR(res, 3.0, 1.0e-11);
-//}
+TEST(utils, length_returns_valid_length_for_many) {
+    // Arrange
+    char **strings = new char*[2];
 
-class Environment : public ::testing::Environment {
-public:
-    ~Environment() override = default;
+    strings[0] = (char*)"Hello";
+    strings[1] = (char*)"World";
 
-    void SetUp() override {}
+    // Act
+    int expected_length = length(strings);
+
+    // Assert
+    ASSERT_EQ(expected_length, 11);
+}
 
 
-    void TearDown() override {}
-};
+TEST(utils, length_returns_one_for_empty_array) {
+    // Arrange
+    char **strings = new char*[0];
+
+    // Act
+    int expected_length = length(strings);
+
+    // Assert
+    ASSERT_EQ(expected_length, 1);
+}
