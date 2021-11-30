44f28c47d41bde08b2b42156bdc95499e4093f53
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-12-01 00:07:22 +0200

    remove redundant code

--- src/utils/watch_utils.h
+++ src/utils/watch_utils.h
@@ -7,11 +7,3 @@
 const char *get_usage();
 
 int contains_argument(const char *short_argument, const char *long_argument, const char *actual_argument);
-
-int contains_short_argument(const char *short_argument, const char *actual_argument);
-
-/**
- *
- * @param m_sec time to delay a thread in milliseconds
- */
-void thread_sleep(long m_sec);
